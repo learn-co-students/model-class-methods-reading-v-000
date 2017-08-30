c44cf2333b26f0915bb3165a49cf8cad6d7cd0c5
Margarita Khodzka <mkhodzka@gmail.com>
2017-08-30 21:07:50 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,18 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @authors = Author.all
+    if !params[:author].blank?
+      @posts = Post.by_author(params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.from_today
+      else
+        @posts = Post.old_news
+      end
+    else
+      @posts = Post.all
+    end
   end
 
   def show
