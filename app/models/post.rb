92388e73d4693d35ef4d44c77b6694eb02d7eedb
Lisa Huang <lisa.yc.huang@gmail.com>
2017-12-14 16:03:25 -0600

    Refactored filter logic, moved from posts_controller to post.rb model file.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,19 @@ class Post < ActiveRecord::Base
   before_validation :make_title_case 
   belongs_to :author
 
-  #put new code here
+  #class methods for filter logic
+
+  def self.by_author(author_id)
+    where(author: author_id)
+  end
+
+  def self.from_today
+    where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
+   
+  def self.old_news
+    where("created_at <?", Time.zone.today.beginning_of_day)
+  end
 
   private
 
