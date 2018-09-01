801caaec02836c37ff3e0660d7e98c5b0419394a
Brennan Fulmer <brennan.flmr@gmail.com>
2018-09-01 04:58:23 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,21 @@
+
 class PostsController < ApplicationController
+  helper_method :params
 
   def index
-    @posts = Post.all
+    @authors = Author.all
+
+    if !params[:author].blank?
+      @posts = Post.by_author(params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.from_today
+      else
+        @posts = Post.old_news
+      end
+    else
+      @posts = Post.all
+    end
   end
 
   def show
@@ -27,4 +41,5 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
   end
+  
 end
