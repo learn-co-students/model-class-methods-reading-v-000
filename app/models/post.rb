04f168cee6c949e2c15b768a2e7e4facfd16002b
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-09-08 15:34:38 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,17 +4,27 @@ class Post < ActiveRecord::Base
   before_validation :make_title_case 
   belongs_to :author
 
-  #put new code here
+  def self.by_author(author_id)
+    where(author: author_id)
+  end
+  
+  def self.from_today
+    where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
+   
+  def self.old_news
+    where("created_at <?", Time.zone.today.beginning_of_day)
+  end
 
   private
 
-  def is_title_case
-    if title.split.any?{|w|w[0].upcase != w[0]}
-      errors.add(:title, "Title must be in title case")
+    def is_title_case
+      if title.split.any?{|w|w[0].upcase != w[0]}
+        errors.add(:title, "Title must be in title case")
+      end
     end
-  end
-
-  def make_title_case
-    self.title = self.title.titlecase
-  end
+  
+    def make_title_case
+      self.title = self.title.titlecase
+    end
 end
