af4fb90a38ec9ba35625068583c4a2a7ebc994f6
Mary Jack <maryjackinthebox@gmail.com>
2018-11-11 05:52:20 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController
 
-  def index
+  def index 
+    @authors = Author.all
+     if !params[:author].blank?
+      @posts = Post.by_author(params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.from_today
+      else
+        @posts = Post.old_news
+      end
+    else
     @posts = Post.all
   end
 
