f439fbce2fadd7a77ec20642881922c347caf988
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-01-27 00:11:34 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,12 @@ class PostsController < ApplicationController
   def index
     @authors = Author.all?
      if !params[:author].blank?
-       @posts = Post.where(author: params[:author])
+       @posts = Post.by_author(params[:author])
        elsif !params[:date].blank?
        if params[:date] == "Today"
-         @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+         @posts = Post.from_today
        else
-         @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+         @posts = Post.old_news
        end
      end
     @posts = Post.all
