96c8816f3545deddb2fb3ffb5a91677f6010a571
github username <github email address>
2019-03-24 20:50:17 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,16 @@ class PostsController < ApplicationController
 
 
   def index
-    @posts = Post.all
     @authors = Author.all
 
     if !params[:author].blank?
      @posts = Post.by_author(params[:author])
     elsif !params[:date].blank?
-     if params[:date] == "Today"
+      if params[:date] == "Today"
        @posts = Post.from_today
-     else
+      else
        @posts = Post.old_news
-     end
+      end
     else
      @posts = Post.all
     end
