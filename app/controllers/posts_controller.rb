615dfd36c42d3deb4981337a098c2df9679f3783
Eva Navarro <emanao@me.com>
2020-04-20 16:32:38 +0000

    Fix bug when filtering posts by authors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,9 @@ class PostsController < ApplicationController
   def index
     # provide a list of authors to the view for the filter control
    @authors = Author.all
-
    # filter the @posts list based on user input
    if !params[:author].blank?
-     @posts = Post.by_author
+     @posts = Post.by_author(params[:author])
    elsif !params[:date].blank?
      if params[:date] == "Today"
        @posts = Post.from_today
